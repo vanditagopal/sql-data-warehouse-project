854ae520b46ee3e122fd470a87776cc8466917d2
Vandita Gopal <121680569+vanditagopal@users.noreply.github.com>
2025-11-19 23:03:01 +0530

    Create quality_checks_gold.sql

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,24 @@
+
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
